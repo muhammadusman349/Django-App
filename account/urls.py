96fe5967ad2f31bf9e7b22a9db6e2bb18c11893e
muhammadusman349 <usmankhadim.uk.uk@gmail.com>
2024-07-25 15:49:27 +0500

    added index page

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path("", index, name="index"),
     path('signup/', SignupView.as_view() , name="list-create-user-view"),
     path('signin/', SigninView.as_view() , name="sigin-view")
 ]
